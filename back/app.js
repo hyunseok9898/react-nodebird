bd192eec80c6beccbabd10d41995a89b1298977a
hyunseok9898 <139948998+hyunseok9898@users.noreply.github.com>
2026-01-08 22:57:59 +0900

    back 서버 포트 수정

--- back/app.js
+++ back/app.js
@@ -64,6 +64,9 @@ app.use("/posts", postsRouter);
 app.use("/user", userRouter);
 app.use("/hashtag", hashtagRouter);
 
-app.listen(3065, () => {
-  console.log("서버 실행 중!");
+const PORT = 3065;
+const HOST = process.env.NODE_ENV === "production" ? "0.0.0.0" : "localhost";
+
+app.listen(PORT, HOST, () => {
+  console.log(`서버 실행 중! ${HOST}:${PORT}`);
 });
